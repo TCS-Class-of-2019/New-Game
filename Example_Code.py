574d4a8c59a94450a012596662b1e542443a95aa
MattZ45986 <33816924+MattZ45986@users.noreply.github.com>
2018-04-19 18:08:05 -0400

    Update and rename Example Code to Example_Code.py

--- Example_Code.py
+++ Example_Code.py
@@ -1,25 +1,36 @@
-# Code based on that of the pygame programming tutorial at pythonprogramming.net
-
 import pygame
+import random
 
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Bob's Adventure")
+pygame.display.set_caption("Coin Collector")
 
-done = False
+crashed = False
 
 bob = pygame.image.load('Bob.png')
+coin = pygame.image.load('Coin.png')
 
 def move(x,y):
     gameDisplay.blit(bob, (x,y))
+
+def makeNewCoin():
+    x, y = random.randint(0,780), random.randint(0,580)
+    gameDisplay.blit(coin, (x,y))
+    return x,y
+
+def displayCoin(x,y):
+    gameDisplay.blit(coin, (x, y))
 
 x = 360
 y = 480
 xMove = 0
 yMove = 0
+score = 0
 
-while not done:
+cx, cy = makeNewCoin()
+
+while not crashed:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -45,8 +56,14 @@ while not done:
     if x < 0: x = 0
     if y > 540: y = 540
     if y < 0: y = 0
-    
     gameDisplay.fill((1,1,1))
+
+    if cx + 20 > x and cx < x + 30 and cy + 20 > y and cy < y + 60:
+        score += 10
+        cx, cy = makeNewCoin()
+    else: displayCoin(cx,cy)
+    
+    
     move(x,y)
 
 
