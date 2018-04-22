3b1759d408cc6a646adc50b6c0e2d86e4dd771dc
MattZ45986 <33816924+MattZ45986@users.noreply.github.com>
2018-04-22 13:53:00 -0400

    Update Tower-Defense-Prototype.py

--- Example Code/Tower-Defense-Prototype.py
+++ Example Code/Tower-Defense-Prototype.py
@@ -12,14 +12,17 @@ pygame.display.set_caption("Tower Defense Prototype")
 
 crashed = False
 
-bob = pygame.image.load('Bob.png')
 coin = pygame.image.load('Dot.png')
 class Dot(object):
-    def __init__(self,points):
+    def __init__(self,points,speed=1):
         self.points = points
         self.point = 0
-        self.pos = self.points[self.point]
+        self.pos = list(self.points[self.point])
         self.img = coin
+        self.speed = speed
+        if self.speed > 1: self.speed = 1
+        elif self.speed < .1: self.speed = .1
+        
 
     def display(self):
         if self.point == len(points)-1:
@@ -29,12 +32,12 @@ class Dot(object):
             xD = self.points[self.point + 1][0] - self.points[self.point][0]
             if xD == 0:
                 x = 0
-                if yD < 0: y = -1
-                elif yD > 0: y = 1
+                if yD < 0: y = -1 * self.speed
+                elif yD > 0: y = 1 * self.speed
                 else: y = 0
             else:
-                y = yD * (1/math.sqrt(yD ** 2 + xD ** 2))
-                x = xD * (1/math.sqrt(yD ** 2 + xD ** 2))
+                y = self.speed * yD * (1/math.sqrt(yD ** 2 + xD ** 2))
+                x = self.speed * xD * (1/math.sqrt(yD ** 2 + xD ** 2))
             self.pos[0] += x
             self.pos[1] += y
         gameDisplay.blit(self.img, [int(round(self.pos[0])),int(round(self.pos[1]))])
@@ -42,22 +45,33 @@ class Dot(object):
         return False
 c = 0
 done = False
-points = [[10,50],[600,50],[600,550],[100,550],[100,150],[400,150],[400,400],[200,400],[200,200],[300,300]]
-listy = [Dot(points)]
+points =((10,50),(600,50),(600,550),(100,550),(100,150),(400,150),(400,400),(200,400),(200,200),(300,300))
+parameter = list(points)
+listy = []
 pointlist = []
-for pair in [[10,50],[600,50],[600,550],[100,550],[100,150],[400,150],[400,400],[200,400],[200,200],[300,300]]:
+for pair in points:
     pointlist.append((pair[0]+15, pair[1]+15))
 while done == False:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
-            listy.append(Dot([[10,50],[600,50],[600,550],[100,550],[100,150],[400,150],[400,400],[200,400],[200,200],[300,300]]))
+            parameter = list(points)
+            if event.key == pygame.K_1: listy.append(Dot(parameter,.1))
+            elif event.key == pygame.K_2: listy.append(Dot(parameter,.2))
+            elif event.key == pygame.K_3: listy.append(Dot(parameter,.3))
+            elif event.key == pygame.K_4: listy.append(Dot(parameter,.4))
+            elif event.key == pygame.K_5: listy.append(Dot(parameter,.5))
+            elif event.key == pygame.K_6: listy.append(Dot(parameter,.6))
+            elif event.key == pygame.K_7: listy.append(Dot(parameter,.7))
+            elif event.key == pygame.K_8: listy.append(Dot(parameter,.8))
+            elif event.key == pygame.K_9: listy.append(Dot(parameter,.9))
+            else: listy.append(Dot(parameter,1))
     gameDisplay.fill((0,0,0))
     pygame.draw.lines(gameDisplay,(255,153,51),False,pointlist,10)
     for dot in listy:
         if dot is not None:
-            if dot.display(): listy.pop(0)
+            if dot.display(): listy.remove(dot)
     pygame.display.update()
 
     
