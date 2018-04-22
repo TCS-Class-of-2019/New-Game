89706a0ff1cf393c87547dad0dd240c3c4fe79de
MattZ45986 <33816924+MattZ45986@users.noreply.github.com>
2018-04-22 17:00:03 -0400

    Update Tower-Defense-Prototype.py

--- Example Code/Tower-Defense-Prototype.py
+++ Example Code/Tower-Defense-Prototype.py
@@ -7,12 +7,10 @@ import math
 pygame.init()
 
 gameDisplay = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Tower Defense Prototype")
-
-
-crashed = False
+pygame.display.set_caption("Tower Defense")
 
 coin = pygame.image.load('Dot.png')
+
 class Dot(object):
     def __init__(self,points,speed=1):
         self.points = points
@@ -21,11 +19,10 @@ class Dot(object):
         self.img = coin
         self.speed = speed
         if self.speed > 1: self.speed = 1
-        elif self.speed < .1: self.speed = .1
-        
+        elif self.speed < .1: self.speed = .1      
 
     def display(self):
-        if self.point == len(points)-1:
+        if self.point == len(self.points)-1:
             return True
         else:
             yD = self.points[self.point + 1][1] - self.points[self.point][1]
@@ -41,41 +38,76 @@ class Dot(object):
             self.pos[0] += x
             self.pos[1] += y
         gameDisplay.blit(self.img, [int(round(self.pos[0])),int(round(self.pos[1]))])
-        if round(self.pos[0]) == self.points[self.point + 1][0] and round(self.pos[1]) == self.points[self.point + 1][1]: self.point += 1
+        xNow = round(self.pos[0])
+        xGoal = self.points[self.point + 1][0]
+        yNow = round(self.pos[1])
+        yGoal = self.points[self.point + 1][1]
+        if (xNow ==  xGoal and  yNow == yGoal) or (self.pos[0] > self.points[self.point +1][0] and x > 0) or (self.pos[0] < self.points[self.point +1][0] and x < 0):
+            self.point += 1
+            self.pos = list(self.points[self.point])
         return False
-c = 0
-done = False
-points =((10,50),(600,50),(600,550),(100,550),(100,150),(400,150),(400,400),(200,400),(200,200),(300,300))
-parameter = list(points)
-listy = []
-pointlist = []
-for pair in points:
-    pointlist.append((pair[0]+15, pair[1]+15))
-while done == False:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        if event.type == pygame.KEYDOWN:
-            parameter = list(points)
-            if event.key == pygame.K_1: listy.append(Dot(parameter,.1))
-            elif event.key == pygame.K_2: listy.append(Dot(parameter,.2))
-            elif event.key == pygame.K_3: listy.append(Dot(parameter,.3))
-            elif event.key == pygame.K_4: listy.append(Dot(parameter,.4))
-            elif event.key == pygame.K_5: listy.append(Dot(parameter,.5))
-            elif event.key == pygame.K_6: listy.append(Dot(parameter,.6))
-            elif event.key == pygame.K_7: listy.append(Dot(parameter,.7))
-            elif event.key == pygame.K_8: listy.append(Dot(parameter,.8))
-            elif event.key == pygame.K_9: listy.append(Dot(parameter,.9))
-            else: listy.append(Dot(parameter,1))
-    gameDisplay.fill((0,0,0))
-    pygame.draw.lines(gameDisplay,(255,153,51),False,pointlist,10)
-    for dot in listy:
-        if dot is not None:
-            if dot.display(): listy.remove(dot)
-    pygame.display.update()
 
-    
-    
+def intro():
+    done = False
+    while not done:
+        gameDisplay.fill((0,0,0))
+        mouse = pygame.mouse.get_pos()
+        if 330 < mouse[0] < 470 and 250 < mouse[1] < 350:  
+            pygame.draw.rect(gameDisplay,(0,255,0), (330,250,140,100))
+        else:
+            pygame.draw.rect(gameDisplay,(255,0,0), (330,250,140,100))
+        font = pygame.font.Font('freesansbold.ttf',50)
+        TextSurf = font.render("Start",True,(127,127,127))
+        TextRect = TextSurf.get_rect() 
+        TextRect.center = ((400),(300))
+        gameDisplay.blit(TextSurf, TextRect)
+        font = pygame.font.Font('freesansbold.ttf',80)
+        TextSurf = font.render("TOWER DEFENSE",True,(127,127,127))
+        TextRect = TextSurf.get_rect()
+        TextRect.center = ((400),(150))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 330 < event.pos[0] < 470 and 250 < event.pos[1] < 350: done = True
+
+def gameLoop():
+    c = 0
+    done = False
+    points =((10,50),(600,50),(600,550),(100,550),(100,150),(400,150),(400,400),(200,400),(200,200),(300,300),(500,300),(300,100),(700,100),(700,570),(50,570),(320,345))
+    parameter = list(points)
+    listy = []
+    pointlist = []
+    for pair in points:
+        pointlist.append((pair[0]+15, pair[1]+15))
+    while done == False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                parameter = list(points)
+                if event.key == pygame.K_1: listy.append(Dot(parameter,.1))
+                elif event.key == pygame.K_2: listy.append(Dot(parameter,.2))
+                elif event.key == pygame.K_3: listy.append(Dot(parameter,.3))
+                elif event.key == pygame.K_4: listy.append(Dot(parameter,.4))
+                elif event.key == pygame.K_5: listy.append(Dot(parameter,.5))
+                elif event.key == pygame.K_6: listy.append(Dot(parameter,.6))
+                elif event.key == pygame.K_7: listy.append(Dot(parameter,.7))
+                elif event.key == pygame.K_8: listy.append(Dot(parameter,.8))
+                elif event.key == pygame.K_9: listy.append(Dot(parameter,.9))
+                else: listy.append(Dot(parameter,1))
+        gameDisplay.fill((0,0,0))
+        pygame.draw.lines(gameDisplay,(255,153,51),False,pointlist,10)
+        for dot in listy:
+            if dot is not None:
+                if dot.display(): listy.remove(dot)
+        pygame.display.update()
+
+intro()
+gameLoop()
 
 pygame.quit()
 quit()
